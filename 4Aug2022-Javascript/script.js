bcfdfa35d7e25c230c6cac41ae4d795c2b0a440e
SmridhiSoni <smridhi1730.be20@chitkara.edu.in>
2022-08-09 02:12:47 +0530

    Js

--- 4Aug2022-Javascript/script.js
+++ 4Aug2022-Javascript/script.js
@@ -29,3 +29,20 @@ switch (new Date().getDay()) {
 
 console.log(dayName); // present day 
 document.getElementById("p1").innerHTML="Today is "+dayName;
+function fnpro(){
+    let l=document.getElementById("l").value;
+    let r=document.getElementById("r").value;
+    let k=document.getElementById("k").value;
+    l=Number(l);
+    r=Number(r);
+    k=Number(k);
+    let count=0;
+    for( let i=l;i<=r;i++){
+  if(i%k==0){
+    count++;
+  }
+
+    }
+    document.getElementById("demo").innerHTML=count;
+
+}
